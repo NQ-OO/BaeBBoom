ee22c74f77c4713825e7a939d0ec8b052459d619
dbsgur <kkr431@naver.com>
2022-03-29 20:10:55 +0900

    hyeok - main, detail, craeteOrder page -- front part

--- app.py
+++ app.py
@@ -12,28 +12,28 @@ def home():
 
 @app.route('/register')
 def register():
-    return 'This is 등록페이지!'
+    return render_template('register.html')
 
 # 상세페이지
 
 
 @app.route('/spec')
 def spec():
-    return 'This is 상세페이지!'
+    return render_template('detail.html')
 
 # 로그인 페이지
 
 
 @app.route('/login')
 def login():
-    return 'This is 로그인페이지!'
+    return render_template('signIn.html')
 
 # 회원가입 페이지
 
 
 @app.route('/signup')
 def signup():
-    return 'This is 회원가입페이지!'
+    return render_template('signUp.html')
 
 
 if __name__ == '__main__':
