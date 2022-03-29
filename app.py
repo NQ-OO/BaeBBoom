b80ac61e0bc514306ef5a02c860deb7696f2b0f8
NQ-OO <lol1219@snu.ac.kr>
2022-03-29 20:36:17 +0900

    [chore] Commit to pull

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+# from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect 
+from forms import SignupForm
+# from wtforms import StringField, submitField
+# from wtforms.validators import DataRequired
+
+
 app = Flask(__name__)
+print(app.config)
 
 
 # 메인페이지
 @app.route('/')
 def home():
-
   return render_template('index.html')
 
 # 등록페이지
@@ -25,17 +32,33 @@ def spec():
 # 로그인 페이지
 
 
-@app.route('/login')
+@app.route('/signin')
 def login():
-    return 'This is 로그인페이지!'
+    return render_template('signin.html')
 
 # 회원가입 페이지
 
 
 @app.route('/signup')
 def signup():
-    return 'This is 회원가입페이지!'
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
+    app.config['SECRET_KEY'] = "secret_key"
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+    
+    
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+#Invalid URL
+@app.errorhandler(404) 
+def page_not_found(e):
+  return render_template("404.html"), 404
+
+
+
+    
+  
